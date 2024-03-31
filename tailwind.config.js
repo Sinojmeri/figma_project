17d4a74e200576b1b06395881af3c3817fc82600
Sinojmeri <sinojmeri.arlind@gmail.com>
2024-03-31 15:28:37 +0200

    First commit at Figma Project

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./**/*.html"],
+  ignore: ["node_modules", "dist", "public"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
+
