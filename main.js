2b0f8181925ffb9fc3d38455c5d9106e3c11bb81
Sinojmeri <sinojmeri.arlind@gmail.com>
2024-05-08 16:32:50 +0200

    Navbar fixed for small screen

--- main.js
+++ main.js
@@ -1,13 +1,19 @@
 let vizat = document.getElementById('vizat');
 let lista = document.getElementById('lista');
+let container = document.getElementById('container');
+document.addEventListener("DOMContentLoaded", () => {
+    container.classList.remove('relative');
+    lista.classList.remove("absolute");
+});
 vizat.addEventListener('click', function () {
     lista.classList.remove('hidden');
-    lista.classList.add('flex', 'absolute', 'flex-col');
+    lista.classList.add('flex', 'absolute' ,'flex-col');
 });
 document.addEventListener('click', function (event) {
     const outside_click = !vizat.contains(event.target) && !lista.contains(event.target);
     if (outside_click) {
         lista.classList.add('hidden');
         lista.classList.remove('flex', 'absolute', 'flex-col');
+        container.classList.remove('relative');
     }
 });
