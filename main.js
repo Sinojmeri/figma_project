09f453b2f5fe72522836bfec3da1fd800f3ad5b7
Sinojmeri <sinojmeri.arlind@gmail.com>
2024-05-11 21:47:20 +0200

    Page completed and responsive

--- main.js
+++ main.js
@@ -1,13 +1,23 @@
 let vizat = document.getElementById('vizat');
 let lista = document.getElementById('lista');
 let container = document.getElementById('container');
+let visibleList = true;
 document.addEventListener("DOMContentLoaded", () => {
     container.classList.remove('relative');
     lista.classList.remove("absolute");
 });
 vizat.addEventListener('click', function () {
+    if (visibleList) {
     lista.classList.remove('hidden');
-    lista.classList.add('flex', 'absolute' ,'flex-col');
+    lista.classList.add('flex', 'absolute', 'flex-col');
+    visibleList = false;  
+    }
+    else {
+        lista.classList.add('hidden');
+        lista.classList.remove('flex', 'absolute', 'flex-col');
+        container.classList.remove('relative');
+        visibleList = true;
+    }
 });
 document.addEventListener('click', function (event) {
     const outside_click = !vizat.contains(event.target) && !lista.contains(event.target);
@@ -15,5 +25,6 @@ document.addEventListener('click', function (event) {
         lista.classList.add('hidden');
         lista.classList.remove('flex', 'absolute', 'flex-col');
         container.classList.remove('relative');
+        visibleList = true;
     }
 });
